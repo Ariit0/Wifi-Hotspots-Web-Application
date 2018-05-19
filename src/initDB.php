612894cf03f47c9c2c2fb2d527bd10420aea61ab
Ari Luangamath <luangamath.ari@gmail.com>
2018-05-20 05:14:25 +1000

    Added a comment

--- src/initDB.php
+++ src/initDB.php
@@ -1,4 +1,5 @@
 			<?php
+				// tests connection to database and catches potential errors during connection
 				try {
 					$pdo = new PDO("mysql:host=cab230.sef.qut.edu.au;dbname=n9446826;", "n9446826", "Zc2eaaddt5yy1");
 					$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
