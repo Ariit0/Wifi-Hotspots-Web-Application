9dfb9a32eaefc08b8e430e0656400cc21d08f3a4
Ari Luangamath <luangamath.ari@gmail.com>
2018-05-21 23:55:04 +1000

    (WIP) Implemented search bar suburb option dropdown

--- src/displaySearchBar.php
+++ src/displaySearchBar.php
@@ -1,22 +1,32 @@
 <form class="searchinput" action="results.php">
+	<input type="text" name="search" placeholder="Enter address, rating or hotspot name">
+	<select>
 
 	<?php
+		// truncates any characters after the specified delimiter
+		function truncateStringAfter($string, $delim) { 
+		    return substr($string, 0, strpos($string, $delim));
+		}
+
 		include "include/initDB.php";
 
 		try {
-			
+			$result = $pdo->query('SELECT DISTINCT suburb FROM items ORDER BY suburb');
+
+			foreach ($result as $suburb) {
+				if (strpos($suburb['suburb'], ',')) { // checks if the string has a comma
+					echo '<option>',truncateStringAfter($suburb['suburb'], ','),'</option>'; // truncate after the specified delimiter
+				} else {
+					echo '<option>', $suburb['suburb'],'</option>';
+				}
+
+			}
 		} catch (Exception $e) {
-			
+			echo $e->getMessage();
 		}
 
 
 	?>
-	<input type="text" name="search" placeholder="Enter address, rating or hotspot name">
-	<select>
-		  <option value="Annerley">Annerley</option>
-		  <option value="Chermisde">Chermisde</option>
-		  <option value="Banyo">Banyo</option>
-		  <option value="Ashgrove">Ashgrove</option>
 	</select>
 	<button type="submit"><i class="fa fa-search"></i></button>
 </form>
