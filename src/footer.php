1753f00213641ec3556dbe776971be9f6f2f701b
Ari Luangamath <luangamath.ari@gmail.com>
2018-05-21 16:52:30 +1000

    Created Footer php file

---
+++ src/footer.php
@@ -0,0 +1,6 @@
+<?php
+		echo "<div id=\"footer\">
+				<p>Ari Luangamath (n9446826) & George Delosa (n9751696) </p>
+				<a href=\"https://github.com/Ariit0/CAB230-Assessment\" target=\"_blank\"> Link to GitHub Repo</a>
+			</div>";
+?>
