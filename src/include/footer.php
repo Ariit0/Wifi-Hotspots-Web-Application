adf89bf8c303fc54aa679d7e1fdc325e644460df
Ari Luangamath <luangamath.ari@gmail.com>
2018-05-21 18:55:43 +1000

    Created header and footer include files

---
+++ src/include/footer.php
@@ -0,0 +1,4 @@
+<div id="footer">
+	<p>Ari Luangamath (n9446826) & George Delosa (n9751696) </p>
+	<a href="https://github.com/Ariit0/CAB230-Assessment" target="_blank"> Link to GitHub Repo</a>
+</div>
---
+++ src/include/header.php
@@ -0,0 +1,14 @@
+
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<!-- using fontawesome for icon fonts -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+		<script type="text/javascript" src="js/login.js"></script>
+		<script type="text/javascript" src="js/register.js"></script>
+		<script type="text/javascript" src="js/results.js"></script>
+		<title><?php echo $title; ?></title>
+	</head>
+
