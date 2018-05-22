d02aa74682f329508a6a0e1aff05eb4bc4cb1f33
Ari <luangamath.ari@gmail.com>
2018-05-22 18:27:36 +1000

    Separated Nav bar from index

---
+++ src/displayNavBar.php
@@ -0,0 +1,11 @@
+<nav id="nav_centre">	
+	<div id="logo"><img src="img/logo.png" id="logoImg"></div>
+	<ul id="menu">	
+		<li><a href="index.php" class="selected">Home</a></li> 
+		<li><a href="aboutus.php">About</a></li> 
+		<li><a href="contactus.php">Contact</a></li>
+		<li><a href="login.php">Login</a></li> 
+		<li><a href="register.php">Register</a></li> 
+	</ul>
+</nav>
+
