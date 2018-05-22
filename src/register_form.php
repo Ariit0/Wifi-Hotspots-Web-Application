ec2371ffcbe590bafffd3a61ab8733a5f372d9fd
George Delosa <georgedel1@outlook.com>
2018-05-23 07:19:37 +1000

    Name changes

--- src/register_form.php
+++ src/register_form.php
@@ -6,43 +6,43 @@
 	<p>
         <?php echo $server_response_msg; ?>
         First name* <br>
-        <input id="firstname" type="text" onchange="OnChangeElement('firstnameError');" onkeypress="OnChangeElement('firstnameError');">
+        <input id="firstname" name="firstname" type="text" onchange="OnChangeElement('firstnameErrorID');" onkeypress="OnChangeElement('firstnameErrorID');">
         <span id="firstnameErrorID" class="error-message"> First name error goes here.</span>
 	</p>
 
 	<p>
         Last name <br>
-        <input id="lastname" type="text" onchange="OnChangeElement('lastnameError');" onkeypress="OnChangeElement('lastnameError');">
+        <input id="lastname" name="lastname" type="text" onchange="OnChangeElement('lastnameErrorID');" onkeypress="OnChangeElement('lastnameErrorID');">
         <span id="lastnameErrorID" class="error-message"> Last name error goes here.</span>
 	</p>
 
 	<p>
         DoB <br>
-        <input id="dob" type="text" placeholder="dd/mm/yyyy" onchange="OnChangeElement('dobError');" onkeypress="OnChangeElement('dobError');">
+        <input id="dob" name="dob" type="text" placeholder="dd/mm/yyyy" onchange="OnChangeElement('dobErrorID');" onkeypress="OnChangeElement('dobErrorID');">
         <span id="dobErrorID" class="error-message"> Date of birth error goes here.</span>
 	</p>
 
 	<p>
         Mobile number <br>
-        <input id="mobile" type="text" placeholder="0123456789" onchange="OnChangeElement('mobilenumError');" onkeypress="OnChangeElement('mobilenumError');">
+        <input id="mobile" name="mobile" type="text" placeholder="0123456789" onchange="OnChangeElement('mobilenumErrorID');" onkeypress="OnChangeElement('mobilenumErrorID');">
         <span id="mobilenumErrorID" class="error-message"> Mobile number error goes here.</span>
 	</p>
 
 	<p>
         Email* <br> 
-        <input id="email" type="text" onchange="OnChangeElement('emailError');" onkeypress="OnChangeElement('emailError');">
+        <input id="email" name="email" type="text" onchange="OnChangeElement('emailErrorID');" onkeypress="OnChangeElement('emailErrorID');">
         <span id="emailErrorID" class="error-message"> Email error goes here.</span>
 	</p>
 
 	<p>
         Password* <br>
-        <input type="password" name="password1" onchange="OnChangeElement('passwordError');" onkeypress="OnChangeElement('passwordError');">
+        <input type="password" name="password1" onchange="OnChangeElement('passwordErrorID');" onkeypress="OnChangeElement('passwordErrorID');">
         <span id="passwordErrorID" class="error-message"> Password error goes here.</span>
     </p>
 
     <p>
         Confirm Password* <br>
-        <input type="password" name="password2" onchange="OnChangeElement('passwordmatchError');" onkeypress="OnChangeElement('passwordmatchError');">
+        <input type="password" name="password2" onchange="OnChangeElement('passwordmatchErrorID');" onkeypress="OnChangeElement('passwordmatchErrorID');">
         <span id="passwordmatchErrorID" class="error-message"> Password match error goes here.</span>
 	</p>
 
