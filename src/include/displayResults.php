3ccf6f4de632987c81d8d3df6d9c06e5f9a0d314
Ari <luangamath.ari@gmail.com>
2018-05-24 20:09:00 +1000

    Improved search result appearance

--- src/include/displayResults.php
+++ src/include/displayResults.php
@@ -1,14 +1,25 @@
 <?php
 	function displayResults($searchQuery) {
-		$_id = 0;
-		foreach ($searchQuery as $suburb) {
-			echo "<div class=\"results\" id = \"res$_id\">";
-			echo "<a href=\"sampleitem.php\"><h1>".$suburb['name']."</h1>";
-			echo "<p>".$suburb['address']."";
-			echo ", ".$suburb['suburb']. "</p></a>";
-			echo "</div> <!-- end results -->";
-			$_id++;
+		$columnCount = -1;
+
+		echo "<div class=\"resultBody\">";
+		foreach ($searchQuery as $suburb) { // 3 columns per row
+			$columnCount++;
+			if (($columnCount % 3) == 0) {
+				echo "<div class=\"resultRow\">";
+			}
+				echo "<div class=\"results\">";
+				echo "<a href=\"sampleitem.php\"><h1>".$suburb['name']."</h1>";
+				echo "<p>".$suburb['address']."";
+				echo ", ".$suburb['suburb']. "</p></a>";
+				echo "</div> <!-- end results -->";
+
+			if ($columnCount == 2) {
+				echo "</div> <!-- end resultsrow -->";
+				$columnCount = -1;
+			}
 		}
+		echo "</div><!-- end resultsbody -->";
 	}
 
 	function displayNoResult() {
