fbe1fa622bf12fdb28006a06648eb202467cba12
George Delosa <georgedel1@outlook.com>
2018-05-24 16:22:16 +1000

    Added placeholder profile page

---
+++ src/profile.php
@@ -0,0 +1,35 @@
+<?php 
+	session_start(); 
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php
+			$title = "My Profile";
+			include "include/header.php";
+		?>
+	</head>
+
+	<body>
+		<div class="nav_bar">
+			<?php
+				include "displayNavBar.php";
+			?>
+		</div>
+
+		<div id="wrapper">
+			<div id="header">
+				<h1>My Profile</h1>
+			</div>
+					
+			<div id="content">
+				Welcome to my profile! There's nothing here yet...
+			</div>
+			
+			<?php
+				include "include/footer.php";
+			?>
+		</div>
+	</body>
+</html>
