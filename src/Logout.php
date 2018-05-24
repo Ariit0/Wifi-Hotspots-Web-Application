a4ae569a5dc58f541b269a9758a39131a71d8771
George Delosa <georgedel1@outlook.com>
2018-05-24 16:22:31 +1000

    Added logout page

--- src/Logout.php
+++ src/Logout.php
@@ -1,5 +1,36 @@
-<?php
-    session_start();
-    unset($_SESSION['LoggedInEmail']);
-    header("Location: http://{$_SERVER['HTTP_HOST']}/src/index.php");
+<?php 
+    session_start(); 
+    unset($_SESSION['LoggedInEmail']); 
 ?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php
+			$title = "Logout";
+			include "include/header.php";
+		?>
+	</head>
+
+	<body>
+		<div class="nav_bar">
+			<?php
+				include "displayNavBar.php";
+			?>
+		</div>
+
+		<div id="wrapper">
+			<div id="header">
+				<h1>See you next time.......</h1>
+			</div>
+					
+			<div id="content">
+				Successfully logged out.
+			</div>
+			
+			<?php
+				include "include/footer.php";
+			?>
+		</div>
+	</body>
+</html>
