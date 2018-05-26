96ce9374c474472ce0f4b3fb1bb6aa0edd77e07a
George Delosa <georgedel1@outlook.com>
2018-05-27 04:32:44 +1000

    Added ability to click marker to access item

--- src/js/results.js
+++ src/js/results.js
@@ -25,6 +25,17 @@ function storeName() {
 
   return names;
 }
+// stores id data
+function storeID() {
+  var listID = document.getElementsByClassName("resultLatLngs");
+  var IDs = [];
+
+  for (var i = 0; i < getTotalLocations(); i++) {
+    IDs.push(listID[i].getAttribute('data-id'));     // add ids to array
+  }
+
+  return IDs;
+}
 // initialises google maps
 function initMap() {
   var map = new google.maps.Map(document.getElementById('initMap'), {
@@ -36,35 +47,45 @@ function initMap() {
 }
 // responsible for setting all markers on the map
 function setMarkers(map) {
-  var latLng = storeLatLng();
-  var name = storeName();
+  var latLngArray = storeLatLng();
+  var nameArray = storeName();
+  var idArray = storeID(); 
 
   var lats = [];
   var lngs = [];
 
-  for (var i = 0; i < latLng.length; i++) {   // split lat and long into separate arrays 
-      var split = latLng[i].split(" "); 
+  for (var i = 0; i < latLngArray.length; i++) {   // split lat and long into separate arrays 
+      var split = latLngArray[i].split(" "); 
       lats.push(split[0]);
       lngs.push(split[1]);
   }
 
   var infowindow = new google.maps.InfoWindow; // initalises a popup info window to appear
 
-  for (var i = 0; i < latLng.length; i++) { // assigns are marker at the designated positon
+  for (var i = 0; i < latLngArray.length; i++) { // assigns are marker at the designated positon
     marker = new google.maps.Marker ({
         position: {lat: parseFloat(lats[i]), lng: parseFloat(lngs[i])}, // convert string vars to float
         map: map,
-        title: name[i],
+        id: idArray[i],
+        title: nameArray[i],
         zIndex: i
     });
     // event listener to check if the marker has been clicked the info window will appear above the marker
     google.maps.event.addListener(marker, 'click', (function(marker, i) { 
          return function() {
-             infowindow.setContent(name[i]);
-             infowindow.open(map, marker);
+             postIDMarker(marker.getPosition().lat(), marker.getPosition().lng(), marker.getTitle(), marker.id);
          }
     })(marker, i));
   }
+}
+
+// used to pass information of clicked marker to next page
+function postIDMarker(lat, lng, itemName, itemID) {
+  document.getElementById('hidden-itemlat').value = lat;
+  document.getElementById('hidden-itemlng').value = lng;
+  document.getElementById('hidden-itemname').value = itemName;
+  document.getElementById('hidden-itemid').value = itemID;
+  document.getElementById('searchResults').submit();
 }
 
 // used to pass information of clicked item to next page
