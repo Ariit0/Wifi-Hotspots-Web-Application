02e44d4b0015724911bc75a221f255ced2be0b23
Ari <luangamath.ari@gmail.com>
2018-05-27 04:16:23 +1000

    More fixes to align with HTML5 validation

--- src/include/review_form.php
+++ src/include/review_form.php
@@ -2,17 +2,17 @@
 	<p>
         Rating*<br>
         <select name="rating" class= 'ratingfilter'>
-        	<option value="1">1&nbsp;&#xf005;</option><br>
-        	<option value="2">2&nbsp;&#xf005;</option><br>
-        	<option value="3">3&nbsp;&#xf005;</option><br>
-        	<option value="4">4&nbsp;&#xf005;</option><br>
-        	<option value="5">5&nbsp;&#xf005;</option><br>
+        	<option value="1">1&nbsp;&#xf005;</option>
+        	<option value="2">2&nbsp;&#xf005;</option>
+        	<option value="3">3&nbsp;&#xf005;</option>
+        	<option value="4">4&nbsp;&#xf005;</option>
+        	<option value="5">5&nbsp;&#xf005;</option>
         </select><br><br>
 	</p>
 
 	<p>
         Description* <br>
-        <textarea id="description" name="description" rows="6" cols="30" onchange="OnChangeElement('descriptionError')";" onkeypress="OnChangeElement('descriptionError')";"></textarea>
+        <textarea id="description" name="description" rows="6" cols="30" onchange="OnChangeElement('descriptionError');" onkeypress="OnChangeElement('descriptionError');"></textarea>
         <span id="descriptionError" class="error-message"> Description error goes here.</span><br>
 	</p>
 
