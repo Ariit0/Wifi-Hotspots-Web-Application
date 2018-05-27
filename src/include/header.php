16e85c4ae65f9c9c5064d5c681f041e028c1ccd6
Ari <luangamath.ari@gmail.com>
2018-05-27 23:25:53 +1000

    Mobile manifest and meta data

--- src/include/header.php
+++ src/include/header.php
@@ -5,7 +5,13 @@
 <!-- using fontawesome for icon fonts -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
+<!-- mobile meta data -->
+<link rel="manifest" href="manifest.json">
+<meta name="description" content="CAB230 Assignment">
+<meta name="keywords" content="HTML, CSS, PHP, JavaScript">
+<meta name="author" content="Ari Luangamath & George Delosa">
+
 <?php 
-	define("SITE_PATH", "Location: http://{$_SERVER['HTTP_HOST']}/cab230/src");
+	define("SITE_PATH", "Location: http://{$_SERVER['HTTP_HOST']}/yelpclone");
 	session_start(); 
 ?>
