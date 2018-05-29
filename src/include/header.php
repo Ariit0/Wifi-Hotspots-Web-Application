0b871ccbb29457e26066fbf08865d947fc4aa67c
Ari <luangamath.ari@gmail.com>
2018-05-29 17:45:02 +1000

    Moved manifest files and added favicon

--- src/include/header.php
+++ src/include/header.php
@@ -6,10 +6,13 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 <!-- mobile meta data -->
-<link rel="manifest" href="manifest.json">
+<link rel="manifest" href="data/manifest.json">
 <meta name="description" content="CAB230 Assignment">
 <meta name="keywords" content="HTML, CSS, PHP, JavaScript">
 <meta name="author" content="Ari Luangamath & George Delosa">
+
+<link rel="icon" href="img/favicon.ico">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
 <?php 
 	define("SITE_PATH", "Location: http://{$_SERVER['HTTP_HOST']}/whatever");
