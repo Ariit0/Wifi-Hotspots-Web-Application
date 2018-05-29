bc6c26f63dd8fde47256f3036b3ab3ee057df2f6
George Delosa <georgedel1@outlook.com>
2018-05-29 16:59:56 +1000

    Added comment to login_form

--- src/include/login_form.php
+++ src/include/login_form.php
@@ -4,7 +4,9 @@
 	<br><br>
 
 	<p>
-		<span id="serverResponseErrorID" style="color:red"><?php echo $server_response_msg; ?></span>
+		<!-- If there was a server-side validation error, display it here. -->
+		<span style="color:red"><?php echo $server_response_msg; ?></span>
+		
         Email <br>
         <input id="emailaddress" name="emailaddress" type="text" value="<?php if(isset($emailaddress)) echo $emailaddress; ?>" 
         onchange="OnChangeElement('emailErrorID');" onkeypress="OnChangeElement('emailErrorID');">
