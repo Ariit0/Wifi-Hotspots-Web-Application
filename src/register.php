7838cd0b052a909a1c7937ca3986eafa88cd8412
George Delosa <georgedel1@outlook.com>
2018-05-29 17:59:19 +1000

    Added comments to register

--- src/register.php
+++ src/register.php
@@ -5,6 +5,7 @@
 		<?php
 			include "include/header.php";
 
+			// Redirect user to the home page if they are logged in (should not be possible to access register page if already logged in)
 			if(isset($_SESSION['userID'])) {
 		    	header(SITE_PATH ."/index.php");
 			}
@@ -29,17 +30,23 @@
 				<?php
 					$server_response_msg = '';
 
+					// If the form from register_form.php was submitted successfully...
 					if (isset($_POST['Register'])) {
 						$firstname = $lastname = $dob = $mobile = $gender = $bio = $email = $password = $confirm_password = '';
 
+						// Validate data on the server-side
 						require 'include/register_server_validation.php';
 						if(ValidateRegisterForm_Server()) {
+
+							// If validation passed, now attempt to register the user.
 							if(TryRegister($firstname, $lastname, $dob, $mobile, $gender, $bio, $email, $password)){
 				            	echo 'Registration successful! <a href="login.php">Click here to login.</a>';
 							} else {
 								echo 'Server error: registration failed, please try again later.';
 							}
 				        } else {
+				        	// If server side validation failed, re-display the form with pre-filled values, alongside a server_response_msg
+				        	// If the server_response_msg is not empty, then it will tell the user what they did wrong, otherwise it is assumed JS was disabled.
 				        	if($server_response_msg === '') {
 				            	$server_response_msg = 'The server has detected invalid input data. Ensure JS is enabled on your browser before submitting for more information.';
 				        	} 
